c179d1773035916c902cabdfd31387f41ca0d5c6
krystiann12345 <83287516+krystiann12345@users.noreply.github.com>
2021-05-02 23:50:45 -0700

    Update Capada-M2A1- C Array.c

--- Capada-M2A1- C Array.c
+++ Capada-M2A1- C Array.c
@@ -2,14 +2,14 @@
 int main()
 {
     int a[1000],i,n,sum=0;
-    printf("size of array:\t");
+    printf("size of array:\t"); //input numbers of array from user//
     scanf("%d",&n);
     for(i=0; i<n;i++)
     {
-    printf("\nEnter Elements %d:\t",i + 1);
+    printf("\nEnter Elements %d:\t",i + 1); //inputted elements from the user//
     scanf("%d",&a[i]);
-    sum=sum + a[i];
+    sum=sum + a[i];  //this will add all the elements the user inputted//
     }
-    printf("\nSum of all array elements:%d\n",sum);
+    printf("\nSum of all array elements:%d\n",sum); //total of elements//
     return 0;
 }
